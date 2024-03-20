455a5f32d7c45fc0842f945f6bd13ff4d1376beb
Tran Van That <85086936+that161@users.noreply.github.com>
2024-03-20 20:57:48 +0700

    change env to deploy

--- express_service/gateway/index.js
+++ express_service/gateway/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/admin-service', proxy('http://localhost:5001'));
-app.use('/application-service', proxy('http://localhost:5002'));
-app.use('/auth-service', proxy('http://localhost:5003'));
-app.use('/company-service', proxy('http://localhost:5004'));
-app.use('/employer-service', proxy('http://localhost:5005'));
-app.use('/job-service', proxy('http://localhost:5006'));
-app.use('/notification-service', proxy('http://localhost:5007'));
-app.use('/user-service', proxy('http://localhost:5008'));
-app.use('/', proxy('http://localhost:5000'));
+app.use('/admin-service', proxy(process.env.URL_ADMIN_SERVICE));
+app.use('/application-service', proxy(process.env.URL_APPLICATION_SERVICE));
+app.use('/auth-service', proxy(process.env.URL_AUTH_SERVICE));
+app.use('/company-service', proxy(process.env.URL_COMPANY_SERVICE));
+app.use('/employer-service', proxy(process.env.URL_EMPLOYER_SERVICE));
+app.use('/job-service', proxy(process.env.URL_JOB_SERVICE));
+app.use('/notification-service', proxy(process.env.URL_NOTIFICATION_SERVICE));
+app.use('/user-service', proxy(process.env.URL_USER_SERVICE));
 
 app.listen(5000, () => {
     console.log('Gateway is listening to port 5000');
