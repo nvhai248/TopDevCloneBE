13dfc5d51ac47075e826985287a3a4ab0d561266
Tran Van That <tranvanthat9a1@gmail.com>
2024-03-30 17:17:15 +0700

    test v1

--- express_service/gateway/index.js
+++ express_service/gateway/index.js
@@ -7,10 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/admin', proxy('http://localhost:5001'));
-app.use('/employer', proxy('http://localhost:5002'));
-app.use('/user', proxy('http://localhost:5003'));
-app.use('/', proxy('http://localhost:5003'));
+app.use('/admin_service', proxy('http://admin_service:5001'));
+app.use('/user_service', proxy('http://user_service:5003'));
 
 app.listen(5000, () => {
     console.log('Gateway is listening to port 5000');
