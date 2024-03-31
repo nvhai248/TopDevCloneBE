bea71a6594d807621b5c54cdfb0fa2ca9f881661
Tran Van That <tranvanthat9a1@gmail.com>
2024-03-31 20:48:46 +0700

    ..

--- express_service/gateway/index.js
+++ express_service/gateway/index.js
@@ -7,8 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/admin_service', proxy('http://admin_service:5001'));
-app.use('/user_service', proxy('http://user_service:5003'));
+app.use('/admin-service', proxy('http://admin_service:5001'));
+app.use('/employer-service', proxy('http://employer_service:5002'));
+app.use('/user-service', proxy('http://user_service:5003'));
+
+app.use('/', (req, res, next) => {
+    return res.status(200).json({ "msg": "Hello from GATEWAY!" });
+})
+
 
 app.listen(5000, () => {
     console.log('Gateway is listening to port 5000');
