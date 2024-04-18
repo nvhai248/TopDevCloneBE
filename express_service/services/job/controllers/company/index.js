59860854684626fc55f6c9241880b69abcecaa12
20120077 <ngqghien@gmail.com>
2024-04-18 21:42:07 +0700

    [add] map repo with controller

--- express_service/services/job/controllers/company/index.js
+++ express_service/services/job/controllers/company/index.js
@@ -3,6 +3,7 @@ const FindCompanyById = require('./find');
 const ListJobsByCompanyId = require('./listJobs');
 const UpdateCompany = require('./update');
 const CreateProduct = require('./createProduct');
+const ListCompanySlider = require('./slider');
 
 class CompanyController {
   // [GET] /companies/:id/jobs
@@ -15,6 +16,8 @@ class CompanyController {
   updateCompany = UpdateCompany;
   // [POST] /companies/:id/product
   createProduct = CreateProduct;
+  // [GET] /companies/slider
+  listCompanySlider = ListCompanySlider;
 }
 
 module.exports = CompanyController;
