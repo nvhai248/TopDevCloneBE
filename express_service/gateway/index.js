0209c6b50c7f69376e2d5cec59f1705c7418dbbf
Huy Pham <pbhuy0909@gmail.com>
2024-04-18 18:29:16 +0700

    fix proxy middleware

--- express_service/gateway/index.js
+++ express_service/gateway/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 require("dotenv").config();
 
@@ -9,7 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/jobs", proxy(process.env.BASE_URL_JOB_SERVICE));
+app.use(
+  "/jobs",
+  createProxyMiddleware({
+    target: process.env.BASE_URL_JOB_SERVICE,
+    changeOrigin: true,
+  })
+);
 app.use("/applications", proxy(process.env.BASE_URL_APPLICATION_SERVICE));
 app.use("/", proxy(process.env.BASE_URL_USER_SERVICE));
 
