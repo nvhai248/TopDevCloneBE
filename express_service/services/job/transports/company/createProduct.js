d78ee09cb65d7e722b9575010d01a2db09a00895
Huy Pham <pbhuy0909@gmail.com>
2024-04-20 18:51:46 +0700

    [fix] create product

--- express_service/services/job/transports/company/createProduct.js
+++ express_service/services/job/transports/company/createProduct.js
@@ -8,12 +8,6 @@ const CreateProduct = async (req, res, next) => {
   const data = req.body;
   const { id } = req.params;
   try {
-    if (!req.file) {
-      data.image = 'https://res.cloudinary.com/dqtcvuae8/image/upload/v1700311688/avatar/ttlmi6rg8pu4m7d4ao6o.png';
-    } else {
-      const result = await uploader(req.file);
-      data.image = result && result.url;
-    }
     const product = await controller.createProduct(id, data);
     SetResponse(res, STATUS_CODES.OK, product, 'OK', null);
   } catch (error) {
