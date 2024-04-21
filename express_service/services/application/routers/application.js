9a178fc5fad4ba5d59d1a95da2a65b8747477198
Tran Van That <tranvanthat9a1@gmail.com>
2024-04-21 15:25:56 +0700

    update

--- express_service/services/application/routers/application.js
+++ express_service/services/application/routers/application.js
@@ -4,11 +4,6 @@ const { ApplicationTransport } = require("../transports");
 const applicationRouter = express.Router();
 const transport = new ApplicationTransport();
 
-// jobRouter.get("/", transport.listJobByConditions);
-// jobRouter.patch("/:id/approve", auth, transport.approveJob);
-// jobRouter.delete("/:id/refuse", auth, transport.refuseJob);
-// jobRouter.get("/:id", auth, transport.findJob);
-// jobRouter.patch("/:id", auth, transport.updateJob);
 applicationRouter.post("/", auth, transport.applyJob);
 applicationRouter.get("/list-apply/:id", auth, transport.listApply);
 applicationRouter.patch("/:id", auth, transport.updateProcessApplication);
