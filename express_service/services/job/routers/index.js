b6c232355878f59b46ea40e5149fae386ea929a1
Tran Van That <tranvanthat9a1@gmail.com>
2024-05-10 23:29:23 +0700

    test

--- express_service/services/job/routers/index.js
+++ express_service/services/job/routers/index.js
@@ -2,6 +2,10 @@ const companyRouter = require("./company");
 const jobRouter = require("./job");
 
 module.exports = (app) => {
+  app.use("/", (req, res, next) => {
+    console.log(`Request to ${req.method} ${req.originalUrl}`);
+    next();
+  });
   app.use("/", jobRouter);
   app.use("/companies", companyRouter);
 };
