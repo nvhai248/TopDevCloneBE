340d7f1e0a05c934b64403eaaa516be3ae8000ea
Truong Ngoc Huy <huy183605@gmail.com>
2024-05-26 04:10:39 +0700

    feat!: update approve many hr status

--- express_service/services/user/transports/admin/index.js
+++ express_service/services/user/transports/admin/index.js
@@ -3,6 +3,9 @@ const CandidateInfo = require('./feats/candidate_info');
 const ListEmployers = require("./feats/employer_list");
 const EmployerInfo = require("./feats/employer_info");
 const UpdateEmployer = require('./feats/employer_update');
+const UpdateHRStatus = require('./feats/employer_approvalStatus');
+const UpdateManyHRStatus = require('./feats/employers_approvalStatus');
+
 
 class AdminTransport {
   /////////////////////////////// CANDIDATE /////////////////////////////////
@@ -23,6 +26,11 @@ class AdminTransport {
   // [PATCH] /admin/employers/:id
   updateEmployer = UpdateEmployer;
 
+  // [PATCH] /admin/employers/updateHRstatus
+  updateHRStatus = UpdateHRStatus;
+
+  // [PATCH] /admin/employers/updateManyHRstatus
+  updateManyHRStatus = UpdateManyHRStatus;
 }
 
 module.exports = AdminTransport;
